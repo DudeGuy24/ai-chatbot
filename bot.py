db49af0e5fe6a194d47b3213f5e2e4512437c502
Bosma, BP, Mr [23537795@sun.ac.za] <23537795@sun.ac.za>
2023-10-12 21:19:48 +0200

    Basic training through listrainer

--- bot.py
+++ bot.py
@@ -1,6 +1,17 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer 
 
 chatbot = ChatBot("Crackbot")
+
+trainer = ListTrainer(chatbot)
+trainer.train ([
+    "Hello",
+    "Whatchu want?"
+])
+trainer.train ([
+    "What's your name?",
+    "Name's Crackbot, what can I do ya for?"
+])
 
 exit_conditions = (":q", "quit", "bye")
 
